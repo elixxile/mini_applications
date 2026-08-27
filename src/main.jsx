189540fbc3b71aa980d6847e7d45ea95b65dd20f
ҽʅιƙρʅιɱ <heis.elikplimm@gmail.com>
2026-08-27 10:11:15 +0000

    Load PLUGIFY mobile responsive styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
+import './responsive.css'
 
 try {
   const redirect = sessionStorage.getItem('plugify-spa-redirect')
