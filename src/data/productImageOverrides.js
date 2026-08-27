a09a2aa086a636134ccff419c2604f7f2f42de06
ҽʅιƙρʅιɱ <heis.elikplimm@gmail.com>
2026-08-27 10:45:55 +0000

    Replace retailer promo flyers with clean product imagery

---
+++ src/data/productImageOverrides.js
@@ -0,0 +1,17 @@
+// Curated clean product renders used when a retailer listing image is a promo flyer
+// or has unsuitable merchandising artwork. Keys match product display names.
+export const productImageOverrides = {
+  'Apple iPhone 17 Pro Max': 'https://www.pngmart.com/files/24/iPhone-17-Pro-Max-PNG.png',
+  'Apple iPhone 17': 'https://pngimg.com/uploads/iphone17/iphone17_PNG11.png',
+  'Samsung Galaxy S26 Ultra': 'https://www.techspecs.info/uploads/SAMSUNG_GALAXY_S26_ULTRA_5_G_97c101e48e.png',
+  'Samsung Galaxy S26': 'https://static2.o9.de/resource/blob/2204160/3f2f61708d146559eafca092cceee474/samsung-galaxy-s26-vs-s25-fe-carousel-gallery-bild-1-data.png',
+  'Samsung Galaxy S26 Plus': 'https://cdn.beebom.com/mobile/samsung-galaxy-s26-plus-front-back-1.png',
+  'Samsung Galaxy A17': 'https://object.pscloud.io/cms/cms/Photo/img_0_77_7186_0_1_WUjMYU.jpg',
+  'Samsung Galaxy A07 LTE': 'https://tabletplaza.vn/images/detailed/30/D1.png',
+  'Samsung Galaxy A57': 'https://cdn11.bigcommerce.com/s-cjn5s2k99e/images/stencil/1280x1280/products/203/1766/Samsung_Galaxy_A57_Gris_BackFront__21141.1778536606.jpg?c=1',
+  'Samsung Galaxy A26': 'https://media.binglee.com.au/cdn-cgi/image/fit%3Dscale-down%2Cf%3Dauto%2Cw%3D1079/d/3/e/b/d3eb4e2250eed0e2819ded335e1075fc44fa32e5_Samsung_SM_A266BZKDXSA_Mobile_Phones_Hero_1.jpg',
+  'Samsung Galaxy A27 5G': 'https://cdn.shopify.com/s/files/1/0641/9388/8321/files/50099730_1079720.png?height=1200&v=1782874909',
+  'Samsung Galaxy A37': 'https://www.whatmobile.com.pk/admin/images/Samsung/Galaxy%20A37/Samsung-Galaxy-A37-Image-2.jpg',
+  'Samsung Galaxy Z Fold 7': 'https://swiftronicsinc.com/cdn/shop/files/Fold7Slv_cce34a21-7836-42cd-962d-f02b589ad6f3.jpg?v=1770053607',
+  'Apple AirPods Pro 3': 'https://ssr.col.movistar.es/api/v3/get-pic/content/dam/movistar/estaticos/imagenes/dispositivos/apple/airpods/airpods-pro-3-blanco-02.png?hash=mm8a9wdd&oe=png'
+}
